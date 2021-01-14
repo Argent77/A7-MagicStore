4fb3327c94ed95f12cb86cb8c5340101f7e5cc38
Argent77 <4519923+Argent77@users.noreply.github.com>
2021-01-14 11:28:21 +0100

    Merchant checks for combat situation

--- A7-MagicStore/dlg/a7_merc.d
+++ A7-MagicStore/dlg/a7_merc.d
@@ -1,6 +1,6 @@
 BEGIN ~A7_MERC~
 
-IF ~Global("A7STO_MerchantSummoned","GLOBAL",0)~ Intro.1
+IF ~Global("A7STO_MerchantSummoned","GLOBAL",0) !ActuallyInCombat()~ Intro.1
   SAY @2000 /* The ghostly shape of a man dressed like a merchant takes form and turns his attention to you. */
   IF ~~ DO ~SetGlobal("A7STO_MerchantSummoned","GLOBAL",1)~ + Intro.2
 END
@@ -13,7 +13,12 @@ IF ~~ Intro.2
   // + ~!Global("A7STO-STO0703","GLOBAL",0)~ + ~"Sorcerous Sundries" by Halbazzer Drin, in "E Baldur's Gate".~ DO ~StartStore("STO0703",LastTalkedToBy(Myself))~ EXIT
 END
 
-IF ~!Global("A7STO_MerchantSummoned","GLOBAL",0)~ Stores.1
+IF ~!Global("A7STO_MerchantSummoned","GLOBAL",0) !ActuallyInCombat()~ Stores.1
   SAY @2002 /* The merchant presents you a list of stores and waits for your decision. */
   COPY_TRANS SAFE ~A7_MERC~ Intro.2
+END
+
+IF ~ActuallyInCombat()~ Combat.1
+  SAY @2004 /* The merchant turns his attention to you. He attempts to present you a list of stores but is too distracted by the hostile atmosphere around you. */
+  IF ~~ EXIT
 END
