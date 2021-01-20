30475c7bdd0a9e3130ae3833332203202e07d039
Argent77 <4519923+Argent77@users.noreply.github.com>
2021-01-20 15:15:10 +0100

    Add more flavor to merchant dialog

--- A7-MagicStore/dlg/a7_merc.d
+++ A7-MagicStore/dlg/a7_merc.d
@@ -1,5 +1,10 @@
 BEGIN ~A7_MERC~
 
+IF ~ActuallyInCombat()~ Combat.1
+  SAY @2004 /* The merchant turns his attention to you. He attempts to present you a list of stores but is too distracted by the hostile atmosphere around you. */
+  IF ~~ EXIT
+END
+
 IF ~Global("A7STO_MerchantSummoned","GLOBAL",0) !ActuallyInCombat()~ Intro.1
   SAY @2000 /* The ghostly shape of a man dressed like a merchant takes form and turns his attention to you. */
 // Responses for each game/campaign
@@ -9,7 +14,5 @@ END
 // Two entries for each game/campaign
 %A7_MERC_GAME_ENTRY%
 
-IF ~ActuallyInCombat()~ Combat.1
-  SAY @2004 /* The merchant turns his attention to you. He attempts to present you a list of stores but is too distracted by the hostile atmosphere around you. */
-  IF ~~ EXIT
-END
+// One entry for each individual store
+%A7_MERC_STORE_ENTRY%
